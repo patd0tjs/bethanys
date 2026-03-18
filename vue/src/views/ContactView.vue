9b2da9c43fd556c94e75c4dee0be3f4095c98b80
patd0tjs <pgbalanza@gmail.com>
2026-03-18 14:33:45 +0800

    services final adjustments

--- vue/src/views/ContactView.vue
+++ vue/src/views/ContactView.vue
@@ -2,20 +2,37 @@
 
 <template>
   <section class="page-section">
-    <div class="container">
-      <h1 class="section-title">Book a Consultation</h1>
+    <div class="container text-center">
+      <div class="contact-info-container">
+        <h1 class="contact-title cursive">Contact Us</h1>
+        
+        <div class="mb-2">
+            <span class="label">Visit Us</span>
+            <p>Poblacion, Valencia, Philippines, 8700</p>
+        </div>
 
-      <form class="booking-form">
-        <input type="text" class="form-control mb-3" placeholder="Full Name" />
-        <input type="email" class="form-control mb-3" placeholder="Email" />
-        <input type="date" class="form-control mb-3" />
-        <textarea
-          class="form-control mb-3"
-          rows="4"
-          placeholder="Tell us about your event"
-        ></textarea>
-        <button class="btn btn-gold" disabled>Submit</button>
-      </form>
+        <div class="mb-2">
+            <span class="label">Call Us</span>
+            <p><a href="tel:+639175663172">+63 917 566 3172</a></p>
+        </div>
+
+        <div class="mb-2">
+            <span class="label">Send Us an Email</span>
+            <p><a href="mailto:hello@bethanys.com">bethanysweddingevents@gmail.com</a></p>
+        </div>
+
+        <div class="mb-5">
+            <span class="label">
+                <a href="https://www.facebook.com/profile.php?id=61584940190349" target="_blank" rel="noopener noreferrer">
+                    Click here to visit our Facebook page
+                </a>
+            </span>
+        </div>
+      </div> 
+      <div class="text-center">
+        <h1 class="contact-title cursive">Our Location</h1>
+        <iframe width="100%" height="500px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=500&amp;hl=en&amp;q=7.903060764555979,125.09087714816481+(Bethany's%20Events)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.mapsdirections.info/pl/mapa-populacji/">ile osób mieszka na danym obszarze</a></iframe>
+      </div>
     </div>
   </section>
 </template>
@@ -23,5 +40,32 @@
 <style scoped>
 .page-section {
   padding: 80px 0;
+}
+
+.contact-title {
+    font-size: 3rem;
+}
+
+.label {
+    display: block;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    font-size: 0.8rem;
+    margin-bottom: 8px;
+    color: #c9a24d;
+}
+
+.page-section a {
+    color: #ffffff;
+    text-decoration: none;
+    transition: color 0.3s ease;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    font-size: 0.8rem;
+    line-height: 1.6
+}
+
+.page-section a:hover {
+    color: #D4AF37;
 }
 </style>
